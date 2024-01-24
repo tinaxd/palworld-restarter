b06c76d414f1eb0249d3545b69e7955e402d5123
tinaxd <31384595+tinaxd@users.noreply.github.com>
2024-01-24 19:26:52 +0900

    Add flake8 configuration and new dependencies
    Add sample executable script
    
    Update Pipfile.lock with new dependencies
    
    Add .gitignore file

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ import os
 import discord
 import asyncio
 import signal
+from discord.ext import tasks
+from loguru import logger
 
 
 def getenv(key: str) -> str:
@@ -14,7 +16,8 @@ def getenv(key: str) -> str:
     return e
 
 
-class MemoryMetrics(dataclasses.dataclass):
+@dataclasses.dataclass
+class MemoryMetrics:
     total: float
     used: float
     swap_total: float
@@ -36,52 +39,111 @@ class PalworldProcess:
         self._proc: asyncio.subprocess.Process | None = None
 
     async def start(self) -> None:
+        logger.info("Requested to start Palworld")
         if self._proc:
+            logger.warning("Palworld is already running")
             return
 
         proc = await asyncio.create_subprocess_exec(
             self.palworld_bin, cwd=self.palworld_workdir
         )
         self._proc = proc
+        logger.info("Palworld started")
 
     async def stop(self) -> None:
+        logger.info("Requested to stop Palworld")
         if not self._proc:
+            logger.warning("Palworld is not running")
             return
 
         # send SIGTERM to process
         self._proc.send_signal(signal.SIGTERM)
+        logger.info("Sent SIGTERM to Palworld")
 
         # wait for process termination
         await self._proc.wait()
+        self._proc = None
+        logger.info("Palworld stopped")
 
 
-class DiscordBot:
+class DiscordBot(discord.Client):
     def __init__(self):
+        super().__init__(intents=discord.Intents.default())
         dotenv.load_dotenv()
         self._discord_token = getenv("DISCORD_TOKEN")
         palworld_bin = getenv("PALWORLD_BIN")
         palworld_workdir = getenv("PALWORLD_WORKDIR")
 
+        guild_id = int(getenv("TARGET_GUILD_ID"))
+        self._target_guild_id = discord.Object(id=guild_id)
+
         self._pp = PalworldProcess(palworld_bin, palworld_workdir)
-        self.client = discord.Client()
 
-    async def start(self):
-        await self.client.login(self._discord_token)
+        self.tree = discord.app_commands.CommandTree(self)
 
-    async def periodic_update(self, period):
-        while True:
-            await self.update_presence()
-            await asyncio.sleep(period)
+    def run(self, *args, **kwargs) -> None:
+        super().run(self._discord_token)
 
+    async def setup_hook(self) -> None:
+        logger.info("Waiting for Palworld to start...")
+        await self._pp.start()
+        logger.info("Palworld started")
+
+        logger.info("Starting background metrics updater...")
+        self.update_presence.start()
+        logger.info("Started background metrics updater")
+
+        logger.info("Registering commands...")
+        self.tree.copy_global_to(guild=self._target_guild_id)
+        await self.tree.sync(guild=self._target_guild_id)
+        logger.info("Registered commands")
+
+    async def restart_server(self):
+        await self._pp.stop()
+        await self._pp.start()
+
+    @tasks.loop(seconds=60.0)
     async def update_presence(self) -> None:
         metrics = MemoryMetrics.fetch()
 
         memory_percent = metrics.used / metrics.total * 100
         swap_percent = metrics.swap_used / metrics.swap_total * 100
 
-        text = f"Memory {memory_percent:.1f}. Swap {swap_percent:.1f}"
+        text = f"Memory: {memory_percent:.1f}%; Swap: {swap_percent:.1f}%"
+        logger.debug(text)
 
         # update presence
-        self.client.change_presence(
+        await self.change_presence(
             status=discord.Status.online, activity=discord.CustomActivity(name=text)
         )
+        logger.debug("Updated presence")
+
+    @update_presence.before_loop
+    async def before_update_presence(self) -> None:
+        await self.wait_until_ready()
+
+    async def on_ready(self) -> None:
+        logger.info("Logged in as {0.user}".format(self))
+
+    async def shutdown(self) -> None:
+        logger.info("Shutting down...")
+        await self._pp.stop()
+        await self.close()
+        logger.info("Shutdown completed")
+
+
+client = DiscordBot()
+
+
+@client.tree.command()
+async def restart_server(interaction: discord.Interaction):
+    logger.info("Received restart_server command")
+    await interaction.response.defer(thinking=True)
+    await client.restart_server()
+    await interaction.followup.send("Restarted server")
+
+
+if __name__ == "__main__":
+    signal.signal(signal.SIGINT, lambda s, k: asyncio.create_task(client.shutdown()))
+    signal.signal(signal.SIGTERM, lambda s, k: asyncio.create_task(client.shutdown()))
+    client.run()
---
+++ test_script/sample_exe.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# show "hello" every 3 seconds
+while true
+do
+    # show current directory
+    echo "hello: $(pwd)"
+    sleep 3
+done
+
+echo "bye bye"
